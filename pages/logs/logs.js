19a38c51e411e7d49fb89ecadba5058e3185ec2a
Dumbass-Kk <429703875@qq.com>
2017-10-09 18:01:48 +0800

    kk
    tab

--- pages/logs/logs.js
+++ pages/logs/logs.js
@@ -3,7 +3,8 @@ const util = require('../../utils/util.js')
 
 Page({
   data: {
-    logs: []
+    logs: [],
+    tst: 'ss'
   },
   onLoad: function () {
     this.setData({
---
+++ pages/settings/person.js
@@ -0,0 +1,25 @@
+const app = getApp()
+Page({
+  data: {
+    motto: 'Hello World',
+    userInfo: {
+      avatarUrl: '../../images/test.png'
+    },
+    hasUserInfo: false,
+    canIUse: wx.canIUse('button.open-type.getUserInfo')
+  },
+  //事件处理函数
+  bindViewTap: function () {
+    wx.navigateTo({
+      url: '../logs/logs'
+    })
+  },
+  onLoad: function () {
+    if (app.globalData.userInfo) {
+      this.setData({
+        userInfo: app.globalData.userInfo,
+        hasUserInfo: true
+      })
+    }
+  }
+})
